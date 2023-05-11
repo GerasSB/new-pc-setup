5ff0c119615ae1365911c38516a92002103897dc
Geras <geras858@gmail.com>
2023-05-10 23:38:35 -0500

    new fzf commands, edited console messages
    • fzf_file to look for files only
    • fzf_cd to cd into selected directory
    • They now print the whole line
    • They now use Write-Host instead of echo
        • Allows to print them with colors

--- terminalSetup.ps1
+++ terminalSetup.ps1
@@ -7,14 +7,19 @@ Get-PoshThemes
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
 PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 Add-Content $PROFILE "Import-Module -Name Terminal-Icons"
-echo Creating 0x0_up alias...
 Add-Content $PROFILE "Import-Module posh-git"
-Add-Content $PROFILE 'function 0x0_up { Param($f) curl -F "file=@$f" https://0x0.st }'
-echo Creating fzf_dir alias...
-Add-Content $PROFILE 'function fzf_dir { Param($dir=".") fd $dir -t d -H -I| fzf | ii }'
-. $PROFILE
 clear
-echo "All that's left is setting up the JetBrains font in the Windows Terminal,"
-echo "changing the theme to One Half Dark, and adjusting opacity"
+Write-Host "Creating 0x0_up alias..." -ForegroundColor Green
+Add-Content $PROFILE 'function 0x0_up { Param($f) curl -F "file=@$f" https://0x0.st }'
+Write-Host "Creating fzf_dir alias..." -ForegroundColor Green
+Add-Content $PROFILE 'function fzf_dir { Param($dir=".") fd $dir -t d -H -I| fzf | ii }'
+Write-Host "Creating fzf_cd alias..." -ForegroundColor Green
+Add-Content $PROFILE 'function fzf_cd { Param($dir=".") fd $dir -t d -H -I| fzf | cd }'
+Write-Host "Creating fzf_file alias..." -ForegroundColor Green
+Add-Content $PROFILE 'function fzf_file { Param($dir=".") fd $dir -t f -H -I| fzf | ii }'
+. $PROFILE
+echo "`n"
+echo "Done! All that's left is setting up the JetBrains font in the Windows"
+echo "Terminal, changing the theme to One Half Dark, and adjusting opacity to 75"
 cmd /c 'pause'
 wt
