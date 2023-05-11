6dae9f03132979e0810a9bf933c9cb7cd9058cd7
Geras <geras858@gmail.com>
2023-05-10 23:38:02 -0500

    added git-posh module

--- terminalSetup.ps1
+++ terminalSetup.ps1
@@ -5,8 +5,10 @@ Get-PoshThemes
 'oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\amro.omp.json" | Invoke-Expression' | Out-File $PROFILE
 . $PROFILE
 Install-Module -Name Terminal-Icons -Repository PSGallery -Force
+PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
 Add-Content $PROFILE "Import-Module -Name Terminal-Icons"
 echo Creating 0x0_up alias...
+Add-Content $PROFILE "Import-Module posh-git"
 Add-Content $PROFILE 'function 0x0_up { Param($f) curl -F "file=@$f" https://0x0.st }'
 echo Creating fzf_dir alias...
 Add-Content $PROFILE 'function fzf_dir { Param($dir=".") fd $dir -t d -H -I| fzf | ii }'
